2ca07481855f212aa3332585cade924759e12b79
SouthinkGaming <southink419@gmail.com>
2023-03-17 10:46:14 -0400

    update

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour {
     // Start is called before the first frame update
-    void Start() {
-        transform.Rotate(0, 0, 45);
-    }
+    void Start() {}
     // Update is called once per frame
-    void Update() {}
+    void Update() {
+        transform.Rotate(0, 0, 0.2f);
+        transform.Translate(0, 0.01f, 0);
+    }
 }
