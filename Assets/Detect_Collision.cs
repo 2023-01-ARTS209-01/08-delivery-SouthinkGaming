21c7e7b7efbffd1fed196d6c8f6fe59d2d96644d
SouthinkGaming <southink419@gmail.com>
2023-03-17 12:32:26 -0400

    speed boost

--- Assets/Detect_Collision.cs
+++ Assets/Detect_Collision.cs
@@ -6,4 +6,7 @@ public class Detect_Collision : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision) {
         Debug.Log("We hit something! ... It was " + collision.gameObject.name);
     }
+    private void OnTriggerEnter2D(Collider2D trigger){
+        Debug.Log("LERRRRROOOIIIIEEE JANNNKKEINS!!!!!!" + trigger.gameObject.name);
+    }
 }
