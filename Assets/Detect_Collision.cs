5d753f47763dc98fc58b2ce4fa0bd3e34695431c
SouthinkGaming <southink419@gmail.com>
2023-03-17 12:20:21 -0400

    update

--- Assets/Detect_Collision.cs
+++ Assets/Detect_Collision.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class Detect_Collision : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision) {
-        
+        Debug.Log("We hit something! ... It was " + collision);
     }
 }
