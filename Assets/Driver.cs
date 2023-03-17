bb2095eeab925bbe9700ee8fe3c44ad9f0c9b877
SouthinkGaming <southink419@gmail.com>
2023-03-17 10:54:54 -0400

    update

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Driver : MonoBehaviour {
     // Controles the stearing speed of the car
     float stearSpeed = 0.1f;
+    // Controles the driving speed of the car
+    float driveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start() {}
     // Update is called once per frame
     void Update() {
         transform.Rotate(0, 0, stearSpeed);
-        transform.Translate(0, 0.01f, 0);
+        transform.Translate(0, driveSpeed, 0);
     }
 }
